967a4eebc069e0f82a07938dd467f83a8e1784a5
ella gortsinskaja <ella.gortsinskaja@ivkhk.ee>
2021-09-16 15:08:48 +0300

    3 version

--- src/mojaaaaaa/Mojaaaaaa.java
+++ src/mojaaaaaa/Mojaaaaaa.java
@@ -15,7 +15,9 @@ public class Mojaaaaaa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("spsppsp");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
     
 }
